122d885c71e939d947f99bbf37c08c8b0c6623e6
David Cid <david.cid@gmail.com>
2020-01-26 23:10:56 +0100

    make the app work with 1 face photo

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,26 @@
 import React from "react";
+import "./FaceRecognition.css";
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
   return (
     <div className="center ma">
-      <div className="center mt2">
-        <img src={imageUrl} alt="" width="700px" height="auto" />
+      <div className="absolute center mt2">
+        <img
+          id="inputimage"
+          src={imageUrl}
+          alt=""
+          width="700px"
+          height="auto"
+        />
+        <div
+          className="bounding-box"
+          style={{
+            top: box.topRow,
+            right: box.rightCol,
+            bottom: box.bottomRow,
+            left: box.leftCol
+          }}
+        ></div>
       </div>
     </div>
   );
