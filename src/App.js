4aba60fef8c0bd47c5a4ec2f929237d6497f36a7
David Cid <david.cid@gmail.com>
2020-01-26 18:25:59 +0100

    setup button and input funcionality

--- src/App.js
+++ src/App.js
@@ -19,6 +19,21 @@ const particlesOptiones = {
 };
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    };
+  }
+
+  onInputChange = event => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log("Click");
+  };
+
   render() {
     return (
       <div className="App">
@@ -26,7 +41,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
         {/* <FaceRecognition /> */}
       </div>
     );
